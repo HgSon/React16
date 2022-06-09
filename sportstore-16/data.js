0e361f0ca25e9fe69b26a5e722c535a0bbefcd67
손한결 <hgson.8@gmail.com>
2022-06-09 14:44:57 +0900

    7장 fake data 변경

--- sportstore-16/data.js
+++ sportstore-16/data.js
@@ -1,10 +1,12 @@
 let faker = require("faker");
-let data = [];
-let categories = ["WaterSports", "Soccer", "Chess", "Running"];
 faker.seed(100);
+
+let categories = ["WaterSports", "Soccer", "Chess"];
+
+let products = []
 for (let i = 1; i <= 503; i++) {
 	let category = faker.helpers.randomize(categories);
-	data.push({
+	products.push({
 		id: i,
 		name: faker.commerce.productName(),
 		category: category,
@@ -13,10 +15,43 @@ for (let i = 1; i <= 503; i++) {
 	})
 }
 
-module.exports = function() {
-	return {
-		categories: categories,
-		products: data,
-		orders: []
+let orders = []
+for (let i = 1; i <= 103; i++) {
+	let fname = faker.name.firstName();
+	let sname = faker.name.lastName();
+	let order = {
+		id: i,
+		name: `${fname} ${sname}`,
+		email: faker.internet.email(fname, sname),
+		address: faker.address.streetAddress(),
+		city: faker.address.city(),
+		zip: faker.address.zipCode(),
+		country: faker.address.country(),
+		shipped: faker.random.boolean(),
+		products: []
 	}
+
+	//상품개수
+	let productCount = faker.random.number({min: 1, max: 5});
+	let product_ids = [];
+
+	//주문한 상품 id들(중복 x)
+	while(product_ids.length < productCount) {
+		let candidateId = faker.random.number({ min: 1, max: products.length});
+
+		if (product_ids.indexOf(candidateId) === -1) {
+			product_ids.push(candidateId)
+		}
+	}
+
+	for (let i = 0; i < productCount; i++) {
+		order.products.push({
+			quantity: faker.random.number({min : 1, max: 10}),
+			product_id: product_ids[i]
+		})
+	}
+
+	orders.push(order);
 }
+
+module.exports = () => ({ categories, products, orders});
